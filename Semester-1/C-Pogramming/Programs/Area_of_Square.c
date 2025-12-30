6bde66467decf914913108cbebf374e8f9326586
Krishna <krishna121729@gmail.com>
2025-12-30 17:42:22 +0530

    Create Area_of_Square.c

---
+++ Semester-1/C-Pogramming/Programs/Area_of_Square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float side,area;
+    printf("Enter side of the square: ");
+    scanf("%f", &side);
+    area = side * side;
+    printf("Area of the square: %f\n", area);
+    
+return 0;
+}
