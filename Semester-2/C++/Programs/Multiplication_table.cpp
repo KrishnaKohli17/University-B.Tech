3ff199a9d5f76add6d2ff08791cfa9bc97289c85
Krishna <krishna121729@gmail.com>
2026-03-29 12:04:53 +0530

    Create Multiplication_table.cpp

---
+++ Semester-2/C++/Programs/Multiplication_table.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+  cout<<"Enter a number: ";
+  cin>>n;
+  for(int i=1;i<=0;i++){
+    cout<<n<<" x "<<i<<" = "<<n*i<endl;}
+  return 0;
+}
