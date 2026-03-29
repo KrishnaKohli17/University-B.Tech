e63ba2e4d0770603991b8fbdf305aa29ba5b64aa
Krishna <krishna121729@gmail.com>
2026-03-29 14:52:59 +0530

    Create Calculate_Area_of_Sqaure.cpp

---
+++ Semester-2/C++/Programs/Calculate_Area_of_Sqaure.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float side, area;
+    cout << "Enter Side of the Square: ";
+    cin >> side;
+    area = side*side;
+    cout << "Area of Square: " << area;
+    return 0;
+}
