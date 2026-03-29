764bfd9b6895d681651afed4fca347924ea30440
Krishna <krishna121729@gmail.com>
2026-03-29 14:35:36 +0530

    Create Find_Area_of_Circle.cpp

---
+++ Semester-2/C++/Programs/Find_Area_of_Circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float radius,area;
+  cout<<"Enter radius of Circle: ";
+  cin>>radius;
+  area=3.14159*radius*radius;
+  cout<<"Area of Circle: "<<area;
+  return 0;
+}
